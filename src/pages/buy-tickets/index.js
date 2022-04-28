4bbe10de9dbba51a69035a549dec932aecb5876b
Pablo <pablo.yncio@immertec.com>
2022-04-28 12:44:29 -0500

    ...

--- src/pages/buy-tickets/index.js
+++ src/pages/buy-tickets/index.js
@@ -2,7 +2,15 @@ import React from "reactn";
 import styled from "styled-components";
 
 export const BuyTickets = (props) => {
-  return <BuyTicketsStyled>hola</BuyTicketsStyled>;
+  return (
+    <BuyTicketsStyled>
+      <div className="min-h-[calc(100vh-50px)] lg:min-h-[calc(100vh-120px)] w-full bg-index bg-no-repeat bg-cover flex lg:items-end bg-bottom">
+        <div className="">
+          <div>Comprar entradas</div>
+        </div>
+      </div>
+    </BuyTicketsStyled>
+  );
 };
 
 const BuyTicketsStyled = styled.div``;
