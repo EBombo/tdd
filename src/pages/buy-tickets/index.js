131bfbaaf0d799a5e5c81f0af5f99a976e701989
Pablo <tech@ebombo.com>
2022-05-03 14:50:26 +0000

    remove uncessary

--- src/pages/buy-tickets/index.js
+++ src/pages/buy-tickets/index.js
@@ -1,31 +1,26 @@
 import React from "reactn";
-import styled from "styled-components";
 import { CulqiComponent } from "./CulqiComponent";
 
 export const BuyTickets = (props) => {
   return (
-    <BuyTicketsStyled>
-      <div className="min-h-[calc(100vh-50px)] lg:min-h-[calc(100vh-120px)] w-full bg-index bg-no-repeat bg-cover flex lg:items-end bg-bottom">
-        <div className="lg:w-[450px] w-[90%] m-auto bg-white/[.60] rounded-md p-6">
-          <div className="text-xl mb-2">Comprar entradas</div>
+    <div className="min-h-[calc(100vh-50px)] lg:min-h-[calc(100vh-120px)] w-full bg-index bg-no-repeat bg-cover flex lg:items-end bg-bottom">
+      <div className="lg:w-[450px] w-[90%] m-auto bg-white/[.60] rounded-md p-6">
+        <div className="text-xl mb-2">Comprar entradas</div>
 
-          <div className="w-[90%] m-auto rounded-md p-6 bg-blackDarken">
-            <div className="text-xl text-primary mb-4 text-center">Entrada Full Access</div>
+        <div className="w-[90%] m-auto rounded-md p-6 bg-blackDarken">
+          <div className="text-xl text-primary mb-4 text-center">Entrada Full Access</div>
 
-            <div className="text-white text-center mb-4">
-              Obtén acceso a toda la feria durante toda su duración y asiste a todas las presentaciones
-            </div>
-
-            <div className="text-white text-center text-7xl text-bold">200</div>
-
-            <div className="text-white text-center text-xl">soles</div>
-
-            <CulqiComponent {...props} />
+          <div className="text-white text-center mb-4">
+            Obtén acceso a toda la feria durante toda su duración y asiste a todas las presentaciones
           </div>
+
+          <div className="text-white text-center text-7xl text-bold">200</div>
+
+          <div className="text-white text-center text-xl">soles</div>
+
+          <CulqiComponent {...props} />
         </div>
       </div>
-    </BuyTicketsStyled>
+    </div>
   );
 };
-
-const BuyTicketsStyled = styled.div``;
