1a5b6951ee4b2a0b3fd08fab7f2e5cd2065d3e41
Pablo <tech@ebombo.com>
2022-05-04 16:31:52 +0000

    new design

--- src/pages/buy-tickets/index.js
+++ src/pages/buy-tickets/index.js
@@ -3,23 +3,22 @@ import { CulqiComponent } from "./CulqiComponent";
 
 export const BuyTickets = (props) => {
   return (
-    <div className="min-h-[calc(100vh-50px)] lg:min-h-[calc(100vh-120px)] w-full bg-index bg-no-repeat bg-cover flex lg:items-end bg-bottom">
-      <div className="lg:w-[450px] w-[90%] m-auto bg-white/[.60] rounded-md p-6">
-        <div className="text-xl mb-2">Comprar entradas</div>
+    <div className="min-h-[calc(100vh-50px)] sm:px-1 lg:min-h-[calc(100vh-120px)] w-full bg-index bg-no-repeat bg-cover flex lg:items-end bg-bottom">
+      <div className="lg:w-[450px] sm:w-[300px] md:mr-[100px] md:ml-auto my-auto rounded-md p-6 bg-blackDarken">
+        <div className="text-xl text-primary mb-4 text-left">Arquirir entrada</div>
 
-        <div className="w-[90%] m-auto rounded-md p-6 bg-blackDarken">
-          <div className="text-xl text-primary mb-4 text-center">Entrada Full Access</div>
-
-          <div className="text-white text-center mb-4">
-            Obtén acceso a toda la feria durante toda su duración y asiste a todas las presentaciones
-          </div>
-
-          <div className="text-white text-center text-7xl text-bold">200</div>
-
-          <div className="text-white text-center text-xl">soles</div>
-
-          <CulqiComponent {...props} />
+        <div className="text-white text-left mb-4">
+          Obtén acceso a toda la feria durante toda su duración y asiste a todas las presentaciones
         </div>
+
+        <div className="text-primary">Precio general</div>
+
+        <div className="text-white text-left text-7xl text-bold flex">
+            200
+            <div className="text-white text-center text-xl">soles</div>
+            </div>
+
+        <CulqiComponent {...props} />
       </div>
     </div>
   );
