1f885d6db0a17477ca00f73e7d5e88368f265145
anargu <anargu@pop-os.localdomain>
2022-05-04 19:54:30 -0500

    hover button

--- src/components/form/Button.js
+++ src/components/form/Button.js
@@ -12,7 +12,7 @@ export const Button = forwardRef((props, ref) => (
         props.disabled
           ? "bg-blackLighten text-gray"
           : props.primary
-          ? "bg-pink-500 text-white hover:bg-pink-700"
+          ? "bg-pink-500 text-white hover:bg-pink-700 hover:border-pink-700"
           : "bg-white text-pink-500  hover:bg-pink-500 hover:border-pink-700 hover:text-white"
       }
       ${props.margin ?? "ml-4 mt-4 lg:mt-0"}
