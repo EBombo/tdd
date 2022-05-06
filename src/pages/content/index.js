3f8a96f80ca76ff5e24233d310af285c65af078c
anargu <anargu@pop-os.localdomain>
2022-05-06 11:38:06 -0500

    fix layout && bg added

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -21,10 +21,10 @@ export const Content = (props) => {
 
   const VideoList = React.memo(() => {
     return (
-      <div className="bg-blackDarken py-4 min-w-[275px] h-full flex lg:block max-h-[150px] lg:max-h-[calc(100vh-120px)] overflow-auto">
+      <div className="bg-blackDarken py-4 min-w-[275px] h-full flex lg:block max-h-[150px] lg:max-h-[calc(100vh-100px)] overflow-auto">
        {videos.map((video, i) => 
          <div key={`video-list-item-${i}`} className="mx-4 mt-2 mb-8 cursor-pointer" onClick={() => setCurrentVideo(video)}>
-            <div className="h-[70px] lg:h-auto overflow-hidden rounded-xl"><Image src={video.portraitUrl} /></div>
+            <div className="h-[70px] min-w-[100px] lg:h-auto overflow-hidden rounded-xl cursor-pointer"><Image src={video.portraitUrl} /></div>
             
             <div className="text-white text-base my-2">{video.title}</div>
          </div>
@@ -34,31 +34,31 @@ export const Content = (props) => {
   });
 
   return (
-    <div className="min-h-[calc(100vh-50px)] lg:min-h-[calc(100vh-120px)] w-full bg-index bg-no-repeat bg-cover">
-      <div className="min-h-[calc(100vh-50px)] lg:min-h-[calc(100vh-120px)] grid lg:grid-cols-[auto_400px] grid-rows-[auto_min-content]">
+    <div className="min-h-[calc(100vh-100px)] w-full bg-content bg-no-repeat bg-cover">
+      <div className="min-h-[calc(100vh-100px)] grid lg:grid-cols-[auto_400px] grid-rows-[auto_min-content]">
 
         <div className="flex flex-col">
           <div className="py-2 lg:min-h-[150px]">
-            <div className="text-xl lg:text-4xl font-bold uppercase my-2">
+            <div className="text-md lg:text-4xl font-bold uppercase my-2">
               <span className="bg-white/[.60] inline-block px-8">I CONGRESO INTERNACIONAL DE TRANSFORMACIÓN DIGITAL</span>
             </div>
-            <div className="text-xl lg:text-2xl font-bold uppercase inline-block">
+            <div className="text-md lg:text-2xl font-bold uppercase inline-block">
               <span className="bg-white/[.60] inline-block px-8">
-                <Image className="inline-block" src={`${config.storageUrl}/resources/film.svg`} width="24px" /> CONTENIDO
+                <Image className="inline-block" src={`${config.storageUrl}/resources/film.svg`} width="18px" desktopWidth="24px" /> CONTENIDO
               </span>
             </div>
           </div>
 
           <div className="bg-white/[.60] h-full">
             <div className="max-w-[1200px] w-full h-full mx-auto flex flex-col">
-              <div className="w-[90%] mx-auto pt-4 text-xl font-bold">{currentVideo.title}</div>
+              <div className="w-[90%] mx-auto pt-4 text-md lg:text-xl font-bold">{currentVideo.title}</div>
               <div className="h-full w-full pt-4 pb-8">
-                <iframe className="mx-auto" width="90%" height="100%" src={currentVideo.embedUrl} title={currentVideo.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe className="mx-auto w-full lg:w-[90%]" height="100%" src={currentVideo.embedUrl} title={currentVideo.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               </div>  
             </div>
           </div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col overflow-x-auto">
           <VideoList />
         </div>
       </div>
