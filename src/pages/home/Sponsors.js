65d7f3112d2bcc5230764f01f0ba25bf0d0e6fa7
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2022-05-06 10:23:21 -0500

    changing sponsors logo size

--- src/pages/home/Sponsors.js
+++ src/pages/home/Sponsors.js
@@ -6,8 +6,8 @@ export const Sponsors = ({ items }) => {
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid lg:grid-cols-6 gap-4 lg:gap-8 mx-4">
       {items.map((item, i) => (
         <Image
-          height="50px"
-          desktopHeight="70px"
+          height="30px"
+          desktopHeight="50px"
           size="contain"
           src={item.imageUrl}
           key={`sponsor-img-${i}`}
