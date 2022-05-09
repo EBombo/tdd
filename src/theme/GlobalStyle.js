539c10dfeac74af985b2b2d15cc62e638710ef02
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2022-05-09 14:07:51 -0500

    fixing overlaying

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -154,7 +154,6 @@ export const GlobalStyle = createGlobalStyle`
   /*-------------- NOTIFICATIONS ANT---------------*/
   .ant-message {
     z-index: 999999;
-    margin-top: 100px;
 
     .ant-message-notice-content {
       background: ${(props) => props.theme.basic.blackDarken};
