59cd4430f7d94343f1bb68c3161fce32099fc3fc
Pablo <pablo.yncio@immertec.com>
2022-05-10 23:40:36 -0500

    update siteMap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_DOMAIN || "https://tdd-dev-348022.web.app",
+  siteUrl: process.env.NEXT_PUBLIC_DOMAIN || "https://citd2022.tdduni.org",
   generateRobotsTxt: true,
 };
