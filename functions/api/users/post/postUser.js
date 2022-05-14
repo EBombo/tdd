8a65f9e4d0635c8274462ecc69098d52f81a1ca6
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2022-05-13 19:00:59 -0500

    sending email to new user

--- functions/api/users/post/postUser.js
+++ functions/api/users/post/postUser.js
@@ -1,6 +1,9 @@
 const logger = require("../../../utils/logger");
 const { updateUser } = require("../../../collections/users");
 const { searchName } = require("../../../utils");
+const { fetchTemplate, fetchSettingsLanding } = require("../../../collections/settings");
+const { sendEmail } = require("../../../email/sendEmail");
+const moment = require("moment");
 
 const postUser = async (req, res, next) => {
   try {
@@ -21,11 +24,26 @@ const postUser = async (req, res, next) => {
 
     await updateUser(user.id, { ...user, searchName: _searchName, createAt: new Date(), updateAt: new Date() });
 
+    await sendEmailToUser({ ...user, searchName: _searchName, createAt: new Date(), updateAt: new Date() })
+
     return res.send({ success: true });
   } catch (error) {
     logger.error(error);
     next(error);
   }
 };
+
+const sendEmailToUser = async (user) => {
+  const welcomeTemplate = await fetchTemplate("welcome");
+  const settings = await fetchSettingsLanding()
+
+  const date = moment(settings.countdown.toDate()).format("DD/MM/YYYY hh:mm:ss")
+
+  logger.log(welcomeTemplate)
+
+  await sendEmail(user.email.trim(), "Bienvenido al TDD", welcomeTemplate, {
+    eventDate: date
+  })
+}
 
 module.exports = { postUser };
