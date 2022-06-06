4594a79a67d6a48f4f85981e5725136b788e42cd
Pablo <pablo.yncio@immertec.com>
2022-06-06 09:16:21 -0500

    iframe event

--- src/pages/event/index.js
+++ src/pages/event/index.js
@@ -9,6 +9,13 @@ export const Event = (props) => {
   return (
     <div className="flex">
       <iframe
+        className="min-h-screen min-w-screen"
+        src="https://storage.net-fs.com/hosting/7319004/0/"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+      {/*
+      <iframe
         src={eventUrl}
         className="min-h-screen w-[70%]"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -19,6 +26,7 @@ export const Event = (props) => {
         className="min-h-screen w-[30%]"
         src={`https://www.youtube.com/live_chat?v=${videoId}&embed_domain=${host}`}
       />
+      */}
     </div>
   );
 };
