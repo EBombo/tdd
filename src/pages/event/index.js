6d0f22156edb235e2afe58edf63f702e60e48015
Pablo <pablo.yncio@immertec.com>
2022-06-06 11:02:46 -0500

    remve unnecessary

--- src/pages/event/index.js
+++ src/pages/event/index.js
@@ -1,32 +1,16 @@
 import React from "reactn";
 
-// TODO replace iframe
-const videoId = "m3cmEq3jRVk";
-const eventUrl = `https://www.youtube.com/embed/${videoId}`;
-const host = "localhost";
+const eventUrl = `https://storage.net-fs.com/hosting/7319004/0/`;
 
 export const Event = (props) => {
   return (
     <div className="flex">
       <iframe
         className="min-h-screen w-full"
-        src="https://storage.net-fs.com/hosting/7319004/0/"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-      {/*
-      <iframe
         src={eventUrl}
-        className="min-h-screen w-[70%]"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       />
-
-      <iframe
-        className="min-h-screen w-[30%]"
-        src={`https://www.youtube.com/live_chat?v=${videoId}&embed_domain=${host}`}
-      />
-      */}
     </div>
   );
 };
