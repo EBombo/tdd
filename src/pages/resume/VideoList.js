14219ab1bbc774e4238c40d9a0c65a49175feda0
Pablo <pablo.yncio@immertec.com>
2022-06-08 17:54:46 -0500

    video frame

--- src/pages/resume/VideoList.js
+++ src/pages/resume/VideoList.js
@@ -5,24 +5,24 @@ export const VideoList = React.memo((props) => {
   return (
     <div className="bg-blackDarken py-4 px-8 grid md:grid-cols-2 lg:grid-cols-4 gap-8 h-full overflow-auto ">
       {props.videos.map((video, i) => (
-        <div key={`video-list-item-${i}`} className="mt-2 mb-8 cursor-pointer">
-          <div className=" min-w-[100px] lg:h-auto overflow-hidden rounded-xl cursor-pointer">
-            <Anchor url={`https://vimeo.com/event/${video.videoId}`} target="_blank">
+        <Anchor url={`https://vimeo.com/event/${video.videoId}`} target="_blank" key={`video-list-item-${i}`}>
+          <div className="mt-2 mb-8 cursor-pointer">
+            <div className=" min-w-[100px] lg:h-auto overflow-hidden rounded-xl cursor-pointer">
               <iframe
                 id="video"
                 width="100%"
                 height="100%"
-                frameBorder="0"
-                allow="autoplay; fullscreen; picture-in-picture"
                 scrolling="no"
+                frameBorder="0"
                 allowFullScreen=""
+                allow="fullscreen; picture-in-picture"
                 src={`https://vimeo.com/event/${video.videoId}/embed`}
               />
-            </Anchor>
-          </div>
+            </div>
 
-          <div className="text-white text-base my-2">{video.title}</div>
-        </div>
+            <div className="text-white text-base my-2">{video.title}</div>
+          </div>
+        </Anchor>
       ))}
     </div>
   );
