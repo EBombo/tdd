8a0d36ae84186c2eb939e5d53b4c5726de628f15
Pablo <pablo.yncio@immertec.com>
2022-06-08 12:17:51 -0500

    new version

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -17,7 +17,7 @@ console.log("process.env.NEXT_PUBLIC_DOMAIN", DOMAIN);
 const CONFIG = process.env.NEXT_PUBLIC_CONFIG ?? "";
 //console.log("process.env.NEXT_PUBLIC_CONFIG", CONFIG);
 
-const version = "0.0.6";
+const version = "0.0.7";
 
 const config = JSON.parse(CONFIG);
 
