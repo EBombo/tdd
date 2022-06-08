612af551a787b40d21819ae5c9eee450ddb6d50d
Pablo <pablo.yncio@immertec.com>
2022-06-08 17:19:19 -0500

    -informacion de evento empezo -ifnormacion de schedule

--- src/pages/event/index.js
+++ src/pages/event/index.js
@@ -1,26 +1,81 @@
 import React, { useState } from "reactn";
 import { Tooltip } from "antd";
 import styled from "styled-components";
-import { config } from "../../firebase";
+import { config, firestore } from "../../firebase";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import moment from "moment";
+import { useInterval } from "../../hooks/useInterval";
+import { useSendError } from "../../hooks";
 
 const eventUrl = `https://storage.net-fs.com/hosting/7319004/0/`;
 
 export const Event = (props) => {
   const router = useRouter();
 
+  const { sendError } = useSendError();
+
   const [eventLink, setEventLink] = useState(eventUrl);
   const [forceRender, setForceRender] = useState(0);
+  const [startEventDate, setStartEventDate] = useState(null);
+  const [endEventDate, setEndEventDate] = useState(null);
+  const [isLive, setIsLive] = useState(false);
 
   useEffect(() => {
     router.prefetch("/resume");
+    router.prefetch("/content");
   }, []);
+
+  useEffect(() => {
+    const fetchSchedule = () =>
+      firestore
+        .collection("settings")
+        .doc("schedule")
+        .onSnapshot((querySnapshotStartDate) => {
+          const schedule = querySnapshotStartDate.data();
+          if (!schedule?.start) return;
+          if (!schedule?.end) return;
+
+          setStartEventDate(schedule.start.toDate());
+          setEndEventDate(schedule.end.toDate());
+        });
+
+    const sub = fetchSchedule();
+    return () => sub && sub();
+  }, []);
+
+  useInterval(() => {
+    try {
+      if (!startEventDate) return;
+
+      const currentDate = moment().utcOffset(-5);
+      const isLive = moment(currentDate).isBetween(startEventDate, endEventDate);
+      setIsLive(isLive);
+    } catch (error) {
+      sendError(error, "useInterval - event");
+    }
+  }, 1000);
 
   return (
     <div className="flex">
+      {startEventDate && (
+        <CountDown>
+          {isLive ? (
+            <>La transmisión ya ha empezado, ve al auditorio!</>
+          ) : (
+            <>La transmisión empieza a las {moment(startEventDate).format("hh:mm a")}</>
+          )}
+        </CountDown>
+      )}
+
+      <Tooltip title="Informacón del evento" placement="left">
+        <FloatIcon {...props} top="15px" onClick={() => router.push("/content")} icon="info-icon.png">
+          <div className="icon" />
+        </FloatIcon>
+      </Tooltip>
+
       <Tooltip title="Videos transmitidos" placement="left">
-        <FloatIcon {...props} top="15px" onClick={() => router.push("/resume")} icon="video-icon.png">
+        <FloatIcon {...props} top="65px" onClick={() => router.push("/resume")} icon="video-icon.png">
           <div className="icon" />
         </FloatIcon>
       </Tooltip>
@@ -29,7 +84,7 @@ export const Event = (props) => {
         <FloatIcon
           {...props}
           icon="home-icon.svg"
-          top="65px"
+          top="115px"
           onClick={() => {
             setEventLink(eventUrl);
             setForceRender(forceRender + 1);
@@ -60,7 +115,8 @@ const FloatIcon = styled.div`
   cursor: pointer;
   padding-left: 5px;
   border-radius: 5px 0 0 5px;
-  background-color: ${(props) => props.theme.basic.primaryLight};
+  box-shadow: 0 3px 2px ${(props) => props.theme.shadow};
+  background-color: ${(props) => props.theme.basic.white};
   ${(props) => (props.top ? `top: ${props.top};` : "top: 80px;")}
 
   .icon {
@@ -70,4 +126,16 @@ const FloatIcon = styled.div`
     background-size: contain;
     background-image: url(${(props) => `${config.storageUrl}/resources/event/${props.icon}`});
   }
+`;
+
+const CountDown = styled.div`
+  top: 0;
+  left: 50%;
+  z-index: 99999;
+  font-size: 10px;
+  padding: 0 10px;
+  position: fixed;
+  margin-left: -100px;
+  border-radius: 0 0 5px 5px;
+  background-color: ${(props) => props.theme.basic.white};
 `;
