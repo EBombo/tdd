72f2061271fc04068487b790af959c696e86f365
Pablo <pablo.yncio@immertec.com>
2022-06-08 21:15:08 -0500

    private resume

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -2,14 +2,15 @@ import React from "reactn";
 import { Resume } from "../../src/pages/resume";
 import Navbar from "../../src/components/Navbar";
 import { SEOMeta } from "../../src/components/common/seo";
+import { PaidUserPrivateRoute } from "../../src/routes/PaidUserPrivateRoute";
 
 const ResumeContainer = (props) => (
-  <>
+  <PaidUserPrivateRoute>
     <SEOMeta {...props} />
     <Navbar>
       <Resume {...props} />
     </Navbar>
-  </>
+  </PaidUserPrivateRoute>
 );
 
 export default ResumeContainer;
